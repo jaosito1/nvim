8a016efb6dda79294f9dc108b2e314c35257dab5
jaosito1 <josuealdana1@gmail.com>
2025-10-29 00:51:57 +0100

    trying oil again

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,16 @@
-return {}
--- return {
--- 	"stevearc/oil.nvim",
--- 	---@module 'oil'
--- 	---@type oil.SetupOpts
--- 	dependencies = { "nvim-tree/nvim-web-devicons" },
--- 	lazy = false,
--- 	opts = {
--- 		keymaps = {
--- 			["q"] = "actions.close",
--- 		},
--- 		view_options = {
--- 			show_hidden = true,
--- 		},
--- 		skip_confirm_for_simple_edits = true,
--- 	},
--- }
+return {
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
+	opts = {
+		keymaps = {
+			["q"] = "actions.close",
+		},
+		view_options = {
+			show_hidden = true,
+		},
+		skip_confirm_for_simple_edits = true,
+	},
+}

ebb2f626b0cd6acc827f02f84ccde572f0c1006d
jaosito1 <josuealdana1@gmail.com>
2025-11-02 01:12:56 +0100

    misc changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,10 +12,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<C-d>", "<C-d>zz")
 vim.keymap.set("v", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 
 vim.keymap.set("n", "<leader>lv", ":vsplit<CR>")
 vim.keymap.set("n", "<leader>lh", ":split<CR>")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -29,10 +29,12 @@ return {
 		-- if a git repo exists
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.git_files, { desc = "Telescope find git files" })
-		vim.keymap.set("n", "<leader>fa", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+
+		vim.keymap.set("n", "<leader>ff", builtin.git_files)
+		vim.keymap.set("n", "<leader>fa", builtin.find_files)
+		vim.keymap.set("n", "<leader>fg", function()
+			builtin.grep_string({ search = vim.fn.input("Grep -> ") })
+		end)
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 		vim.keymap.set("n", "<leader>ft", builtin.colorscheme, { desc = "Telescope colorschemes" })
 	end,

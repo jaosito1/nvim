b79766917a73a95d8b6ffc65e599d24b3f15903b
jaosito1 <josuealdana1@gmail.com>
2025-12-27 13:10:58 +0100

    add tex keybindings and settings

---
+++ after/ftplugin/tex.lua
@@ -0,0 +1,10 @@
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.spell = true
+vim.o.spelllang = "fr"
+
+vim.keymap.set({"n", "v"}, "j", "gj")
+vim.keymap.set({"n", "v"}, "k", "gk")
+
+vim.keymap.set({"n", "v"}, "J", "j")
+vim.keymap.set({"n", "v"}, "K", "k")
